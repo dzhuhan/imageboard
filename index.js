e3074d690b4418b1ba5c6fe0da40b654e888a609
Andrii Dzhuhan <andzhuhan@gmail.com>
2024-06-19 03:12:51 +0200

    Added sample boundaries for pagination and little bug fix

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const page_button_prev = document.getElementById("page-button-prev");
 const page_button_next = document.getElementById("page-button-next");
 const page_button_last = document.getElementById("page-button-last");
 
-let page_count = 0;
+let page_count = 1;
+const page_max = 399;
 
 page_button_first.onclick = function() {
 	page_count = 1;
@@ -14,7 +15,8 @@ page_button_first.onclick = function() {
 }
 
 page_button_prev.onclick = function() {
-	page_count--;
+	if(page_count > 1)
+		page_count--;
 	for(var i = 0; i < 5; i++) {
 		var page_button = document.getElementById("page-button-" + i);
 		page_button.textContent = page_count + i;
@@ -22,6 +24,7 @@ page_button_prev.onclick = function() {
 }
 
 page_button_next.onclick = function() {
+	if(page_count < page_max)
 	page_count++;
 	for(var i = 0; i < 5; i++) {
 		var page_button = document.getElementById("page-button-" + i);
@@ -30,7 +33,7 @@ page_button_next.onclick = function() {
 }
 
 page_button_last.onclick = function() {
-	page_count = 333;
+	page_count = page_max;
 	for(var i = 0; i < 5; i++) {
 		var page_button = document.getElementById("page-button-" + i);
 		page_button.textContent = page_count + i;
